f7835ee0d285b25e7ad7969f8f91904d15807daa
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-07-28 15:50:06 +0300

    add notification data

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,63 @@
     - See hover and focus states for all interactive elements on the page
 */
 
+interface INotification {
+  id: number;
+  img: string;
+  name: string;
+  content: string;
+  time: string;
+  subContent?: string;
+  isMarked: boolean;
+}
+
+const notificationData = [
+  {
+    id: 1,
+    img: "/images/avatar-mark-webber.webp",
+    name: "Mark Webber",
+    content: "reacted to your recent post",
+    subContent: "My first tournament today!",
+    time: "1m ago",
+    isMarked: true,
+  },
+  {
+    id: 2,
+    img: "/images/avatar-angela-gray.webp",
+    name: "Angela Gray",
+    content: "followed you",
+    time: "5m ago",
+    isMarked: true,
+  },
+  {
+    id: 3,
+    img: "/images/avatar-jacob-thompson.webp",
+    name: "Jacob Thompson",
+    content: "has joined your group",
+    subContent: " Chess Club",
+    time: "1day ago",
+    isMarked: true,
+  },
+  {
+    id: 4,
+    img: "/images/avatar-nathan-peterson.webp",
+    name: "Nathan Peterson",
+    content: "reacted to your recent post",
+    subContent: "5 end-game strategies to increase your win rate",
+    time: "2 weeks ago",
+    isMarked: false,
+  },
+  {
+    id: 5,
+    img: "/images/avatar-anna-kim.webp",
+    name: "Anna Kim",
+    content: "left the group",
+    subContent: "Chess Club",
+    time: "2 weeks ago",
+    isMarked: false,
+  },
+];
+
 function App() {
   return (
     <main>
@@ -39,27 +96,36 @@ function NotificationPage() {
         <button className="btn">Mark all as read</button>
       </div>
       <div className="notifications">
-        <Notification />
-        <Notification />
-        <Notification />
+        {notificationData.map((item) => {
+          return (
+            <Notification
+              key={item.id}
+              name={item.name}
+              content={item.content}
+              subContent={item.subContent}
+              img={item.img}
+              time={item.time}
+              isMarked={item.isMarked}
+            />
+          );
+        })}
         <MessageNotification />
         <CommentNotification />
-        <Notification />
-        <Notification />
       </div>
     </div>
   );
 }
 
-function Notification() {
+function Notification({ name, img, content, time, subContent, isMarked }: INotification) {
   return (
-    <div className="notification selected">
-      <img src="images/avatar-mark-webber.webp" alt="" />
+    <div className={`notification ${isMarked && "marked"}`}>
+      <img src={img} alt={name} />
       <div className="content">
         <p>
-          <strong>Mark Webber</strong> reacted to your recent post <span>My first tournament today!</span>
+          <strong>{name}</strong> {content} <span>{subContent}</span>
+          {isMarked && <div className="circle-red"></div>}
         </p>
-        <p>1m ago</p>
+        <p>{time}</p>
       </div>
     </div>
   );
